9f227f0a2ea1486ee57b5a1c42d99954e1c0628b
Technical anon <91734162+technicalanon@users.noreply.github.com>
2026-05-12 14:24:07 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useMemo, useRef, useState } from "react";
 
-const brandLogo = "i.ibb.co/nNGGZ45t/Untitled-design-1.png";
+const brandLogo = "https://i.ibb.co/nNGGZ45t/Untitled-design-1.png";
 
 const statsKey = "mahashiv-enterprises-stats";
 
